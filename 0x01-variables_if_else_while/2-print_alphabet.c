b63034485032ec12aaaa611bf2141f0aeda973f9
IndieFrederick <sfrederick229@gmail.com>
2023-02-17 16:09:30 -0800

    I sometimes suffer from insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in lowercase
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+		putchar(l);
+	putchar('\n');
+
+		return (0);
+}
