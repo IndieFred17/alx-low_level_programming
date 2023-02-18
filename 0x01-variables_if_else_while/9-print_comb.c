7590d1deb4af8495682e33a42303603675684340
IndieFrederick <sfrederick229@gmail.com>
2023-02-18 05:50:27 -0800

    Patience, persistence and perspiration

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,13 +10,11 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n <= 9; n++)
+	for (n = 0; n < 10; n++)
 	{
-		if (n == 9)
-			putchar((n % 10) + '0');
-		else
+		putchar((n % 10) + '0');
+		if (n != 9)
 		{
-			putchar((n % 10) + '0');
 			putchar(',');
 			putchar(' ');
 		}
