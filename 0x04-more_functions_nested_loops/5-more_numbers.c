0ef7b4769c428285135b2b9d7ab73823ded45107
IndieFrederick <sfrederick229@gmail.com>
2023-02-23 06:47:02 -0800

    Universal language

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,11 +10,11 @@ void more_numbers(void)
 {
 	int i, k;
 
-	for (i = 1; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (k = 0; k < 15; k++)
+		for (k = 0; k <= 14; k++)
 		{
-			if (k >= 10)
+			if (k > 9)
 				_putchar(k / 10 + '0');
 				_putchar(k % 10 + '0');
 		}
