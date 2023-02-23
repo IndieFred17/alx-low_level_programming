c056bac9842584106182554d3e338371245c3407
IndieFrederick <sfrederick229@gmail.com>
2023-02-23 06:38:33 -0800

    Universal language

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * followed by a new line
+ * Return: 0-14 ten times
+ */
+
+void more_numbers(void)
+{
+	int i, k;
+
+	for (i = 1; i < 10; i++)
+	{
+		for (k = 0; k < 15; k++)
+		{
+			if (i >= 10)
+				_putchar(k / 10 + '0');
+				_putchar(k % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
