9d705e2c9d0beace884e320709c276aa8c1084df
IndieFrederick <sfrederick229@gmail.com>
2023-03-10 18:53:14 -0800

    Best argument

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,12 +1,12 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - prints all arguments it receives
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return Always 0 (success)
+ * Return: Always 0 (success)
  */
 
 int main(int argc, char *argv[])
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 
 	for (i = 0; i < argc; i++)
 	{
-		printf("%s\n", argv [i]);
+		printf("%s\n", argv[i]);
 	}
 	return (0);
 }
